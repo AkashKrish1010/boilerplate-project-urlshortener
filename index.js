a72f44a49c9b9eeb09b1ac6d0fb8f20d1d3d6ef2
Akash S <akashkrish1010@gmail.com>
2025-06-22 13:43:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+const dotenv = require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
 const app = express();
 
 // Basic Configuration
@@ -8,17 +9,45 @@ const port = process.env.PORT || 3000;
 
 app.use(cors());
 
-app.use('/public', express.static(`${process.cwd()}/public`));
+app.use("/public", express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
-// Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+const urlDatabase = {};
+let counter = 1;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.post("/api/shorturl", function (req, res) {
+  const org_url = req.body.url;
+
+  try {
+    const parsed = new URL(org_url);
+    if (parsed.protocol !== "http:" && parsed.protocol !== "https:") {
+      return res.json({ error: "invalid url" }); 
+    }
+
+    const short_url = counter++;
+    urlDatabase[short_url] = org_url;
+
+    res.json({ original_url: org_url, short_url: short_url });
+  } catch (error) {
+    res.json({ error: "invalid url" }); 
+  }
 });
 
-app.listen(port, function() {
+app.get("/api/shorturl/:short_url", function (req, res) {
+  const short_url = req.params.short_url;
+
+  const originalUrl = urlDatabase[short_url];
+  if (!originalUrl) {
+    return res.json({ error: "invalid url" }); 
+  }
+
+  res.redirect(originalUrl);
+});
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
